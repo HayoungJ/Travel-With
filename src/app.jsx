87de5a42f282f67e059cf02f149e2187edcaa36a
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-06 18:25:21 +0900

    Add react-router-dom
    Made routes for Login page, Register Page, Dashboard Page

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,23 @@
 import styles from './app.module.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/login/login';
+import Dashboard from './components/dashboard/dashboard';
+import Register from './components/register/register';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route path="travel">
+            <Route path=":travelId" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
