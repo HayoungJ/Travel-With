42acac91c772c93f498d1a77715514e5e376c376
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-08 18:12:51 +0900

    Add 'Name' field for user info + Handle error for register

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -3,16 +3,26 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signInWithPopup,
+  updateProfile,
 } from 'firebase/auth';
 
 class AuthService {
-  async register(email, password) {
-    const userCredential = await createUserWithEmailAndPassword(
-      firebaseAuth,
-      email,
-      password
-    );
-    return userCredential.user;
+  async register(name, email, password) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        firebaseAuth,
+        email,
+        password
+      );
+
+      updateProfile(firebaseAuth.currentUser, {
+        displayName: name,
+      });
+
+      return userCredential.user.uid;
+    } catch (error) {
+      alert(this.defineError(error.code));
+    }
   }
 
   async loginWithEmail(email, password) {
@@ -48,6 +58,21 @@ class AuthService {
         return googleProvider;
       default:
         throw new Error(`Unknown login provider ${providerName}`);
+    }
+  }
+
+  defineError(error) {
+    switch (error) {
+      case 'auth/email-already-exists':
+      case 'auth/email-already-in-use':
+        return '이미 사용 중인 이메일 입니다.';
+      case 'auth/invalid-email':
+        return '잘못된 이메일 주소 입니다.';
+      case 'auth/invalid-password':
+      case 'auth/weak-password':
+        return '비밀번호는 6글자 이상이여야 합니다.';
+      default:
+        return `현재 회원가입이 불가능합니다. 사이트 제작자에게 문의해 주세요. Error Code: ${error}`;
     }
   }
 }
