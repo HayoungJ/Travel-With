467a9c9731003249e0f215b1c9276ef216bff8e1
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-08 15:37:44 +0900

    Initialize Firebase app

---
+++ src/service/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(firebaseApp);
+export const googleProvider = new GoogleAuthProvider();
