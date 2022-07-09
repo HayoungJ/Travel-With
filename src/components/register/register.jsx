2852fd036ad2d9f7bb8e80375a7071e87e919d9a
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-09 17:14:22 +0900

    Add name field, Handle register button's status
    Register button will not be activated until every input have data
    To prevent whitespace-only value, prevent typing whitespace at input fields

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import { useRef } from 'react';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styles from './register.module.css';
 
 const Register = ({ authService }) => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const navigate = useNavigate();
 
   const onRegister = async () => {
-    const user = await authService.register(
-      emailRef.current.value,
-      passwordRef.current.value
+    const userId = await authService.register(
+      inputs.name,
+      inputs.email,
+      inputs.password
     );
-    goToTravel(user.uid);
+    goToTravel(userId);
   };
 
   const goToTravel = (userId) => {
     navigate('/travel', { state: { id: userId } });
+  };
+
+  const handleChange = (event) => {
+    event.target.value = event.target.value.trim();
+    const updatedInputs = { ...inputs };
+    updatedInputs[event.target.name] = event.target.value;
+    const updatedIsDisabled =
+      Object.values(updatedInputs).findIndex((el) => el === '') < 0
+        ? false
+        : true;
+    setInputs(updatedInputs);
+    setIsDisabled(updatedIsDisabled);
   };
 
   return (
@@ -29,22 +46,35 @@ const Register = ({ authService }) => {
         </h1>
         <section className={styles.register}>
           <input
-            ref={emailRef}
             className={styles.input}
+            name="name"
             type="text"
-            placeholder="Email"
+            placeholder="이름"
+            onChange={handleChange}
           />
           <input
-            ref={passwordRef}
             className={styles.input}
-            type="password"
-            placeholder="Password"
+            name="email"
+            type="text"
+            placeholder="이메일"
+            onChange={handleChange}
           />
-          <button className={styles.button} onClick={onRegister}>
-            Register
+          <input
+            className={styles.input}
+            name="password"
+            type="password"
+            placeholder="비밀번호 (6글자 이상)"
+            onChange={handleChange}
+          />
+          <button
+            className={styles.button}
+            onClick={onRegister}
+            disabled={isDisabled}
+          >
+            회원가입
           </button>
           <Link to="/" className={styles['link-button']}>
-            기존 계정으로 로그인하고 싶으신가요?
+            기존에 가입한 계정이 있으신가요?
           </Link>
         </section>
       </div>
