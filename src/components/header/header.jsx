7828e52334707fbe00ec42b99bbd254fea208a3f
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-11 19:14:26 +0900

    Add: Header with profile and logout button

---
+++ src/components/header/header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState } from 'react';
+import styles from './header.module.css';
+
+const Header = ({ onLogout, profileUrl, name }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsVisible((isVisible) => !isVisible);
+  };
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.logo}>
+        <span className="logo-style">Travel With</span>
+      </h1>
+      <section className={styles.menu}>
+        <div className={styles.profile} onClick={handleClick}>
+          {profileUrl ? (
+            <img className={styles.image} src={profileUrl} alt="profile" />
+          ) : (
+            <div className={styles.name}>{name ? name[0] : '?'}</div>
+          )}
+        </div>
+        <nav className={`${styles.nav} ${!isVisible && styles.hidden}`}>
+          <button className={styles.button} onClick={onLogout}>
+            로그아웃
+          </button>
+        </nav>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
