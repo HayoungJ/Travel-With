a15588ed5993a2c441cfff7186493436eca83818
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-18 20:46:51 +0900

    Add: travel repository

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.module.css';
 import App from './app';
 import AuthService from './service/auth_service';
+import TravelRepository from './service/travel_repository';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const authService = new AuthService();
+const travelRepository = new TravelRepository();
 
 root.render(
   <React.StrictMode>
-    <App authService={authService} />
+    <App authService={authService} travelRepository={travelRepository} />
   </React.StrictMode>
 );
