60b090d823041177b8b8c067d2192eb58a261069
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-20 20:44:15 +0900

    Add: functions to add and remove nodes to firebase db

--- src/service/travel_repository.js
+++ src/service/travel_repository.js
@@ -1,11 +1,21 @@
-import { onValue, ref, set } from 'firebase/database';
+import { onValue, ref, remove, set } from 'firebase/database';
 import { firebaseDatabase } from './firebase';
 
 class TravelRepository {
   saveTravel(travelId, info) {
-    set(ref(firebaseDatabase, 'travels/' + travelId), {
+    set(ref(firebaseDatabase, `travels/${travelId}`), {
       ...info,
     });
+  }
+
+  saveTravelNode(travelId, node) {
+    set(ref(firebaseDatabase, `travels/${travelId}/nodes/${node.id}`), {
+      ...node,
+    });
+  }
+
+  removeTravelNode(travelId, node) {
+    remove(ref(firebaseDatabase, `travels/${travelId}/nodes/${node.id}`));
   }
 
   syncTravel(travelId, onUpdate) {
