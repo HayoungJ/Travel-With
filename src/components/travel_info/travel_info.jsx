03c7af9127547e60b89a5b89e77d5e11dfac5aea
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-20 20:45:20 +0900

    Add: travel info sections
    사용자가 입력했던 여행 제목, 장소, 날짜를 표시하는 info component
    각 input을 클릭 해 수정할 수 있고, 수정사항은 firebase db에 즉시 반영된다.

---
+++ src/components/travel_info/travel_info.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import styles from './travel_info.module.css';
+import DatePicker from 'react-datepicker';
+
+const TravelInfo = ({ updateTravel, travelInfo }) => {
+  const { title, place } = travelInfo;
+
+  const [startDate, setStartDate] = useState(
+    travelInfo.startDate && new Date(travelInfo.startDate)
+  );
+  const [endDate, setEndDate] = useState(
+    travelInfo.endDate && new Date(travelInfo.endDate)
+  );
+
+  const onChange = (event) => {
+    if (event.currentTarget == null) {
+      return;
+    }
+    event.preventDefault();
+    updateTravel({
+      ...travelInfo,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  useEffect(() => {
+    setStartDate(travelInfo.startDate && new Date(travelInfo.startDate));
+    setEndDate(travelInfo.endDate && new Date(travelInfo.endDate));
+  }, [travelInfo]);
+
+  return (
+    <ul className={styles['info-wrap']}>
+      <li className={styles.info}>
+        <input
+          className={styles.input}
+          name="title"
+          value={title ? title : ''}
+          placeholder="여행 이름"
+          onChange={onChange}
+        />
+      </li>
+      <li className={styles.info}>
+        <input
+          className={styles.input}
+          name="place"
+          value={place ? place : ''}
+          placeholder="여행 장소"
+          onChange={onChange}
+        />
+      </li>
+      <li className={`${styles.info} ${styles.date}`}>
+        <DatePicker
+          wrapperClassName={styles['calendar-wrapper']}
+          className={styles['calendar-input']}
+          name="startDate"
+          selected={startDate}
+          onChange={(date) => {
+            setStartDate(date);
+            date &&
+              updateTravel({
+                ...travelInfo,
+                startDate: date.toString(),
+              });
+          }}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          dateFormat="yyyy/MM/dd"
+          placeholderText="시작일"
+        />
+        <p className={styles['date-deco']}>~</p>
+        <DatePicker
+          wrapperClassName={styles['calendar-wrapper']}
+          className={styles['calendar-input']}
+          name="endDate"
+          selected={endDate}
+          onChange={(date) => {
+            setEndDate(date);
+            date &&
+              updateTravel({
+                ...travelInfo,
+                endDate: date.toString(),
+              });
+          }}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          dateFormat="yyyy/MM/dd"
+          placeholderText="종료일"
+        />
+      </li>
+    </ul>
+  );
+};
+export default TravelInfo;
