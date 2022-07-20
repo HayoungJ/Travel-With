bd5a0cae8661f8e3fb9fdc2ad7e7dd3373aab8a2
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-20 20:46:11 +0900

    Add: buttons for travel selections
    새 여행/기존 여행/링크 입력 중 선택할 수 있는 페이지에서 각 단계를 이동하기 위한 버튼들

--- src/components/select_travel_button/select_travel_button.jsx
+++ src/components/select_travel_button/select_travel_button.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import styles from './select_travel_button.module.css';
 
-const SelectTravelButton = (props) => {
+const SelectTravelButton = ({ handleSelect, handleSubmit }) => {
+  const goToBack = () => {
+    handleSelect('basement');
+  };
+
+  const goToTravelPlan = () => {
+    handleSubmit();
+  };
+
   return (
     <div className={styles.buttons}>
-      <button className={`${styles.button} ${styles.back}`}>뒤로가기</button>
-      <button className={`${styles.button} ${styles.submit}`}>시작하기</button>
+      <button className={`${styles.button} ${styles.back}`} onClick={goToBack}>
+        뒤로가기
+      </button>
+      <button
+        className={`${styles.button} ${styles.submit}`}
+        onClick={goToTravelPlan}
+      >
+        시작하기
+      </button>
     </div>
   );
 };
