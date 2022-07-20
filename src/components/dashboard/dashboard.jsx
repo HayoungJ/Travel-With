7f10ff15dacf9a800a9d7795c5f6dcf51f0dd5f1
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-20 20:48:41 +0900

    Add: 지도와 연동되는 함수들 추가, travel node를 firebase에 연동하는 함수들 추가
    새 지도 생성, 지도 이동, 지도에서 검색, 마커 표시

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Header from '../header/header';
@@ -6,13 +6,60 @@ import Map from '../map/map';
 import Plan from '../plan/plan';
 import styles from './dashboard.module.css';
 
-const Dashboard = ({ travelRepository }) => {
+const Dashboard = ({ travelRepository, kakaoMap }) => {
   const navigate = useNavigate();
   const params = useParams();
 
   const [travelId, setTravelId] = useState(null);
   const [travelInfo, setTravelInfo] = useState({});
-  const [isPlan, setIsPlan] = useState(true);
+  const [buttonSwitch, setButtonSwitch] = useState({
+    isPlan: true,
+    isWallet: false,
+    isSetting: false,
+  });
+  const [map, setMap] = useState();
+  const [mapMarkers, setMapMarkers] = useState({});
+  const [travelNode, setTravelNode] = useState({});
+
+  const createMap = (container) => {
+    const newMap = kakaoMap.createMap(container);
+    setMap(newMap);
+  };
+
+  const repositionMap = (place) => {
+    place && kakaoMap.repositionMap(map, place);
+  };
+
+  const searchAtMap = (keyword, handleMarkerSelect) => {
+    keyword && kakaoMap.search(map, keyword, handleMarkerSelect);
+  };
+
+  const handleDisplayMarker = (marker) => {
+    const updated = { ...mapMarkers };
+
+    if (Object.keys(updated).find((element) => element === marker.id)) {
+      delete updated[marker.id];
+    } else {
+      updated[marker.id] = marker;
+    }
+
+    kakaoMap.createCustomMarker(map, Object.values(updated));
+    setMapMarkers(updated);
+  };
+
+  const createOrUpdateTravelNode = (node) => {
+    travelRepository.saveTravelNode(travelId, node);
+    const updated = { ...travelNode };
+    updated[node.id] = node;
+    setTravelNode(updated);
+  };
+
+  const removeTravelNode = (node) => {
+    travelRepository.removeTravelNode(travelId, node);
+    const updated = { ...travelNode };
+    delete updated[node.id];
+    setTravelNode(updated);
+  };
 
   const updateTravel = (info) => {
     setTravelInfo(info);
@@ -29,6 +76,7 @@ const Dashboard = ({ travelRepository }) => {
     }
     const stopSync = travelRepository.syncTravel(travelId, (data) => {
       data && setTravelInfo(data);
+      data.nodes && setTravelNode(data.nodes);
     });
 
     return () => stopSync;
@@ -38,17 +86,49 @@ const Dashboard = ({ travelRepository }) => {
     <>
       <Header />
       <section className={styles.dashboard}>
-        <Map />
+        <div className={styles.container}>
+          <Map
+            createMap={createMap}
+            repositionMap={repositionMap}
+            updateTravel={updateTravel}
+            travelInfo={travelInfo}
+          />
+        </div>
         <div className={styles.container}>
           <nav className={styles.nav}>
-            <button className={`${styles.button} ${isPlan && styles.active}`}>
+            <button
+              className={`${styles.button} ${
+                buttonSwitch.isPlan && styles.active
+              }`}
+            >
               계획
             </button>
-            <button className={`${styles.button} ${!isPlan && styles.active}`}>
+            <button
+              className={`${styles.button} ${
+                buttonSwitch.isWallet && styles.active
+              }`}
+            >
               지갑
             </button>
+            <button
+              className={`${styles.button} ${
+                buttonSwitch.isSetting && styles.active
+              }`}
+            >
+              설정
+            </button>
           </nav>
-          <Plan updateTravel={updateTravel} travelInfo={travelInfo} />
+          <div className={styles['scroll-area']}>
+            <Plan
+              updateTravel={updateTravel}
+              searchAtMap={searchAtMap}
+              travelInfo={travelInfo}
+              travelNode={travelNode}
+              createOrUpdateTravelNode={createOrUpdateTravelNode}
+              removeTravelNode={removeTravelNode}
+              handleDisplayMarker={handleDisplayMarker}
+            />
+          </div>
         </div>
       </section>
     </>
