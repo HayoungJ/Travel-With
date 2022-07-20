6bb5e122279a2ccacec8bf25d9cf039ca121dd07
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-20 20:41:37 +0900

    Add: Plan section
    새로운 계획을 추가하고, 기존에 등록해둔 계획을 확인할 수 있는 plan component 추가
    Plan component의 데이터는 firebase와 연동되어 있어, 추가 버튼을 눌러 추가하거나 기존 계획을 클릭해 수정할 때 데이터베이스에 반영된다.

--- src/components/plan/plan.jsx
+++ src/components/plan/plan.jsx
@@ -1,98 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import DatePicker from 'react-datepicker';
+import AddTravelNode from '../add_travel_node/add_travel_node';
+import EditTravelNode from '../edit_travel_node/edit_travel_node';
 import styles from './plan.module.css';
 
-const Plan = ({ updateTravel, travelInfo }) => {
-  const { title, place } = travelInfo;
-
-  const [startDate, setStartDate] = useState(
-    travelInfo.startDate && new Date(travelInfo.startDate)
-  );
-  const [endDate, setEndDate] = useState(
-    travelInfo.endDate && new Date(travelInfo.endDate)
-  );
-
-  const onChange = (event) => {
-    if (event.currentTarget == null) {
-      return;
-    }
-    event.preventDefault();
-    updateTravel({
-      ...travelInfo,
-      [event.currentTarget.name]: event.currentTarget.value,
-    });
-  };
-
-  useEffect(() => {
-    setStartDate(travelInfo.startDate && new Date(travelInfo.startDate));
-    setEndDate(travelInfo.endDate && new Date(travelInfo.endDate));
-  }, [travelInfo]);
-
+const Plan = ({
+  searchAtMap,
+  travelNode,
+  createOrUpdateTravelNode,
+  removeTravelNode,
+  handleDisplayMarker,
+}) => {
   return (
     <section className={styles.plan}>
-      <ul className={styles['info-wrap']}>
-        <li className={styles.info}>
-          <input
-            className={styles.input}
-            name="title"
-            value={title ? title : ''}
-            placeholder="여행 이름"
-            onChange={onChange}
+      <ul className={styles.nodes}>
+        <AddTravelNode
+          createTravelNode={createOrUpdateTravelNode}
+          searchAtMap={searchAtMap}
+        />
+        {Object.keys(travelNode).map((key) => (
+          <EditTravelNode
+            key={key}
+            node={travelNode[key]}
+            updateTravelNode={createOrUpdateTravelNode}
+            removeTravelNode={removeTravelNode}
+            searchAtMap={searchAtMap}
+            handleDisplayMarker={handleDisplayMarker}
           />
-        </li>
-        <li className={styles.info}>
-          <input
-            className={styles.input}
-            name="place"
-            value={place ? place : ''}
-            placeholder="여행 장소"
-            onChange={onChange}
-          />
-        </li>
-        <li className={`${styles.info} ${styles.date}`}>
-          <DatePicker
-            wrapperClassName={styles['calendar-wrapper']}
-            className={styles['calendar-input']}
-            name="startDate"
-            selected={startDate}
-            onChange={(date) => {
-              setStartDate(date);
-              date &&
-                updateTravel({
-                  ...travelInfo,
-                  startDate: date.toString(),
-                });
-            }}
-            selectsStart
-            startDate={startDate}
-            endDate={endDate}
-            dateFormat="yyyy/MM/dd"
-            placeholderText="시작일"
-          />
-          <p className={styles['date-deco']}>~</p>
-          <DatePicker
-            wrapperClassName={styles['calendar-wrapper']}
-            className={styles['calendar-input']}
-            name="endDate"
-            selected={endDate}
-            onChange={(date) => {
-              setEndDate(date);
-              date &&
-                updateTravel({
-                  ...travelInfo,
-                  endDate: date.toString(),
-                });
-            }}
-            selectsEnd
-            startDate={startDate}
-            endDate={endDate}
-            minDate={startDate}
-            dateFormat="yyyy/MM/dd"
-            placeholderText="종료일"
-          />
-        </li>
+        ))}
       </ul>
-      <article className={styles.node}></article>
     </section>
   );
 };
