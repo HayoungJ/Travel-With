27219fad7a6fd4844edb97130705a31e0f66a022
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-21 23:33:34 +0900

    Add: 기존의 데이터 불러오기 기능, 데이터 수정 및 firebase db에 업데이트 기능

--- src/components/expenses_list/expenses_list.jsx
+++ src/components/expenses_list/expenses_list.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './expenses_list.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
-const ExpensesList = (props) => {
+const ExpensesList = ({
+  list,
+  updateExpenses,
+  removeExpenses,
+  returnNumber,
+  returnWithFormatAndUnit,
+}) => {
+  const { where, when, category, spent } = list;
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const updated = { ...list };
+    if (event.currentTarget.name === 'spent') {
+      const numberValue = returnNumber(event.currentTarget.value);
+      const stringValue = returnWithFormatAndUnit(numberValue);
+      updated.spent = numberValue;
+      event.currentTarget.value = stringValue;
+    } else updated[event.currentTarget.name] = event.currentTarget.value;
+    updateExpenses(updated);
+  };
+
+  const onRemove = () => {
+    removeExpenses(list);
+  };
+
   return (
     <li className={styles.list}>
       <input
@@ -11,26 +35,34 @@ const ExpensesList = (props) => {
         className={styles.input}
         type="text"
         placeholder="장소"
+        value={where}
+        onChange={onChange}
       />
       <input
         name="when"
         className={styles.input}
         type="text"
         placeholder="날짜"
+        value={when}
+        onChange={onChange}
       />
       <input
         name="category"
         className={styles.input}
         type="text"
         placeholder="분류"
+        value={category}
+        onChange={onChange}
       />
       <input
         name="spent"
         className={styles.input}
         type="text"
         placeholder="금액"
+        value={returnWithFormatAndUnit(spent)}
+        onChange={onChange}
       />
-      <button className={styles.button}>
+      <button className={styles.button} onClick={onRemove}>
         <FontAwesomeIcon icon={faTrashCan} />
       </button>
     </li>
