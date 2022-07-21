00c92ad1f608b0993727b134a1bba27e9390aebb
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-21 23:32:53 +0900

    Add: 새 경비 내역을 추가 기능, 총 사용 금액 계산 및 인당 계산 금액 계산 기능

--- src/components/expenses/expenses.jsx
+++ src/components/expenses/expenses.jsx
@@ -1,59 +1,117 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ExpensesList from '../expenses_list/expenses_list';
 import styles from './expenses.module.css';
 
-const Expenses = (props) => {
+const Expenses = ({ list, createOrUpdateExpenses, removeExpenses }) => {
+  const formRef = useRef();
+  const whereRef = useRef();
+  const whenRef = useRef();
+  const categoryRef = useRef();
+
+  const [peopleNumber, setPeopleNumber] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [spent, setSpent] = useState('');
+
+  const onPeopleNumberChange = (event) => {
+    setPeopleNumber(event.currentTarget.value);
+  };
+
+  const returnNumber = (string) => {
+    const numsStr = string.replace(/[^0-9]/g, '');
+    return Number(numsStr);
+  };
+
+  const returnWithFormatAndUnit = (number) => {
+    const str =
+      typeof number === 'number'
+        ? '₩' + Intl.NumberFormat('en-US').format(number)
+        : '';
+    return str;
+  };
+
+  const onAdd = (event) => {
+    event.preventDefault();
+    createOrUpdateExpenses({
+      id: `expenses-${Date.now()}`,
+      where: whereRef.current.value,
+      when: whenRef.current.value,
+      category: categoryRef.current.value,
+      spent: spent,
+    });
+    formRef.current.reset();
+  };
+
+  useEffect(() => {
+    let updatedTotal = 0;
+    Object.keys(list).forEach((key) => {
+      if (typeof list[key].spent === 'number') updatedTotal += list[key].spent;
+    });
+    setTotal(updatedTotal);
+  }, [list]);
+
   return (
     <section className={styles.expenses}>
       <article className={styles.calculator}>
         <input
           className={styles['people-number']}
           type="number"
-          placeholder="인원수"
+          placeholder="인원 수"
+          onChange={onPeopleNumberChange}
         />
-        <div className={styles['personal-expenses']}>100000원</div>
+        <div className={styles.total}>₩{total}</div>
+        <div className={styles['personal-expenses']}>
+          {peopleNumber > 0
+            ? `₩${total / peopleNumber}`
+            : `인원 수를 입력해주세요`}
+        </div>
       </article>
       <hr className={styles.hr} />
       <article className={styles.table}>
-        <div className={styles.header}>
+        <form ref={formRef} className={styles.header}>
           <input
+            ref={whereRef}
             name="where"
             className={styles.input}
-            type="text"
             placeholder="장소"
           />
           <input
+            ref={whenRef}
             name="when"
             className={styles.input}
-            type="text"
             placeholder="날짜"
           />
           <input
+            ref={categoryRef}
             name="category"
             className={styles.input}
-            type="text"
             placeholder="분류"
           />
           <input
             name="spent"
             className={styles.input}
-            type="text"
             placeholder="금액"
+            onChange={(event) => {
+              const numberValue = returnNumber(event.currentTarget.value);
+              const stringValue = returnWithFormatAndUnit(numberValue);
+              setSpent(numberValue);
+              event.currentTarget.value = stringValue;
+            }}
           />
-          <button className={styles.button}>+</button>
-        </div>
+          <button className={styles.button} onClick={onAdd}>
+            +
+          </button>
+        </form>
         <ul className={styles.body}>
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
-          <ExpensesList />
+          {Object.keys(list).map((key) => (
+            <ExpensesList
+              key={list[key].id}
+              list={list[key]}
+              updateExpenses={createOrUpdateExpenses}
+              removeExpenses={removeExpenses}
+              returnNumber={returnNumber}
+              returnWithFormatAndUnit={returnWithFormatAndUnit}
+            />
+          ))}
         </ul>
       </article>
     </section>
