75c86312e303278e359801c8fe52180ad15553b8
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-24 00:08:02 +0900

    Add: 여행 참여 기능 추가
    사용자가 입력한 링크에서 여행 아이디에 해당하는 부분을 잘라옴
    이후 해당 아이디와 일치하는 여행이 잇는지 확인 후
    있다면 유저 하위의 travel 리스트에 아이디 추가 후
    해당 여행을 확인할 수 있는 대시보드 링크로 이동

--- src/components/participate_travel/participate_travel.jsx
+++ src/components/participate_travel/participate_travel.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SelectTravelButton from '../select_travel_button/select_travel_button';
 import styles from './participate_travel.module.css';
 
-const ParticipateTravel = (props) => {
+const ParticipateTravel = ({ travelRepository, handleSelect, user }) => {
+  const linkRef = useRef();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    const travelId = linkRef.current.value.slice(-13);
+    const travelCheck = await travelRepository.getTravel(travelId);
+
+    if (!travelCheck) {
+      alert('존재하지 않는 여행 계획입니다.');
+      return;
+    }
+
+    travelRepository.saveTravelSubData(travelId, 'editor', user);
+
+    const travelIds = await travelRepository.getUserTravel(user.id);
+    const updated = travelIds ? [...travelIds, travelId] : [travelId];
+    travelRepository.saveUserTravel(user.id, updated);
+
+    navigate(`/travel/${travelId}`);
+  };
+
   return (
     <>
       <input
+        ref={linkRef}
         className={styles.input}
         type="text"
         placeholder="공유받은 링크를 입력해주세요"
       />
-      <SelectTravelButton />
+      <div className={styles['button-wrap']}>
+        <SelectTravelButton
+          handleSelect={handleSelect}
+          handleSubmit={handleSubmit}
+        />
+      </div>
     </>
   );
 };
