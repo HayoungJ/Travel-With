b634ba77edd920dd72a8e69d7ba06db0c5a52f0e
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-24 00:02:36 +0900

    Add: 여행 계획 수정 권한이 있는 사람 수정 기능, 여행 계획 참여하지 않기/삭제 버튼 기능

--- src/components/travel_setting/travel_setting.jsx
+++ src/components/travel_setting/travel_setting.jsx
@@ -1,32 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './travel_setting.module.css';
 
-const TravelSetting = ({ travelId }) => {
+const TravelSetting = ({
+  travelId,
+  editableUser,
+  removeEditor,
+  handlePlanDelete,
+  handleFallOut,
+  isOwner,
+}) => {
+  const travelLink = `http://localhost:3000/travel/${travelId}`;
+
+  const onShare = () => {
+    navigator.clipboard.writeText(travelLink);
+  };
+
+  const onRemove = (id) => {
+    removeEditor(editableUser[id]);
+  };
+
+  const onDelete = () => {
+    handlePlanDelete();
+  };
+
+  const onFallOut = () => {
+    handleFallOut();
+  };
+
   return (
     <section className={styles.setting}>
       <article className={styles.share}>
         <h1 className={styles.title}>공유하기</h1>
-        <p className={styles.link}>https://localhost:3000/travel/{travelId}</p>
+        <p className={styles.link} onClick={onShare}>
+          {travelLink}
+        </p>
       </article>
       <article className={styles.participation}>
         <h1 className={styles.title}>참여자 관리</h1>
         <ul className={styles.table}>
-          <li className={styles.user}>
-            <p className={styles.name}>Hayoung Jeon</p>
-            <button className={styles.remove}>X</button>
-          </li>
-          <li className={styles.user}>
-            <p className={styles.name}>Hayoung Jeon</p>
-            <button className={styles.remove}>X</button>
-          </li>
-          <li className={styles.user}>
-            <p className={styles.name}>Hayoung Jeon</p>
-            <button className={styles.remove}>X</button>
-          </li>
+          {editableUser &&
+            Object.keys(editableUser).map((key) => (
+              <li key={key} className={styles.user}>
+                <p className={styles.name}>{editableUser[key].name}</p>
+                {editableUser[key].owner ? (
+                  <p className={styles.owner}>(소유자)</p>
+                ) : (
+                  <button
+                    className={styles.remove}
+                    onClick={() => onRemove(key)}
+                  >
+                    X
+                  </button>
+                )}
+              </li>
+            ))}
         </ul>
       </article>
       <article className={styles.delete}>
-        <button className={styles.button}>이 계획을 삭제합니다</button>
+        {isOwner ? (
+          <button className={styles.button} onClick={onDelete}>
+            이 계획을 삭제합니다
+          </button>
+        ) : (
+          <button className={styles.button} onClick={onFallOut}>
+            이 계획에 참여하지 않습니다
+          </button>
+        )}
       </article>
     </section>
   );
