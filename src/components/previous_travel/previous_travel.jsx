6ed1388d95b43fd2fc6ac446af9e1e9c401e7c16
Hayoung Jeon <hayoungj0710@gmail.com>
2022-07-24 00:08:53 +0900

    Add: 유저가 생성 혹은 참여 중인 여행 계획 확인
    유저 하위의 travel 리스트에 있는 여행 아이디들을 받아와
    해당 아이디와 일치하는 정보들을 불러온 후
    제목만 화면에 출력

--- src/components/previous_travel/previous_travel.jsx
+++ src/components/previous_travel/previous_travel.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SelectTravelButton from '../select_travel_button/select_travel_button';
 import styles from './previous_travel.module.css';
 
-const PreviousTravel = (props) => {
+const PreviousTravel = ({ handleSelect, travelRepository, user }) => {
+  const navigate = useNavigate();
+
+  const [travelList, setTravelList] = useState([]);
+
+  const getTravelTitleList = async () => {
+    const data = await travelRepository.getUserTravel(user.id);
+
+    data.forEach(async (element) => {
+      const travelInfo = await travelRepository.getTravel(element);
+
+      setTravelList((travelList) => {
+        const updated = [...travelList];
+        updated.push({ title: travelInfo.title, id: travelInfo.id });
+        return updated;
+      });
+    });
+  };
+
+  const onClick = (id) => {
+    navigate(`/travel/${id}`);
+  };
+
+  useEffect(() => {
+    getTravelTitleList();
+  }, []);
+
   return (
     <>
       <ul className={styles.selection}>
-        <li className={styles.option}>속초 여행</li>
-        <li className={styles.option}>광주 여행</li>
-        <li className={styles.option}>제주도 여행</li>
+        {travelList &&
+          travelList.map((info) => (
+            <li
+              key={info.id}
+              className={styles.option}
+              onClick={() => onClick(info.id)}
+            >
+              {info.title}
+            </li>
+          ))}
       </ul>
-      <SelectTravelButton />
+      <div className={styles['button-wrap']}>
+        <SelectTravelButton handleSelect={handleSelect} />
+      </div>
     </>
   );
 };
